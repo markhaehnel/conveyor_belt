92dcc7d323fdccb63adf974187eadc8432a10ade
Mark Hähnel <hello@markhaehnel.de>
2023-08-20 17:24:22 +0200

    chore: fix clippy lint in im-memory example

--- examples/in-memory/main.rs
+++ examples/in-memory/main.rs
@@ -3,7 +3,7 @@ use conveyor_belt::{builder::Builder, retry_strategy::RetryStrategy};
 #[tokio::main]
 async fn main() {
     // use builder to create a queue
-    let queue = Builder::<usize>::new()
+    let _queue = Builder::<usize>::new()
         .name("test".to_string())
         .retry_strategy(RetryStrategy::None)
         .timeout(std::time::Duration::from_secs(1))
